5028e82a64e7ed84f1c08a15390cb5e618261932
vigl2019 <vigl.box@gmail.com>
2020-01-12 21:53:14 +0200

    Code refactoring done

--- src/com/tests/automation/qa/Holubenko_Viacheslav_Task_3.java
+++ src/com/tests/automation/qa/Holubenko_Viacheslav_Task_3.java
@@ -5,19 +5,32 @@ package com.tests.automation.qa;
  */
 
 public class Holubenko_Viacheslav_Task_3 {
+
+    private static int[] arrNumbers = {5, 1, 55, 108, 115, 235, 9, 12, 38, 15};
+
     public static void main(String[] args) {
 
-        int[] arrNumbers = {5, 1, 55, 108, 115, 235, 9, 12, 38, 15};
+        System.out.println("Исходные элементы массива");
+
+        for (int i = 0; i < arrNumbers.length; i++) {
+            printArrElements(i);
+        }
+
+        System.out.printf("%n%n");
         System.out.println("Элементы массива, кратные 3");
 
         for (int i = 0; i < arrNumbers.length; i++) {
             if ((arrNumbers[i] % 3) == 0) {
-                if (i == (arrNumbers.length - 1)) {
-                    System.out.print(arrNumbers[i] + ".");
-                } else {
-                    System.out.print(arrNumbers[i] + "; ");
-                }
+                printArrElements(i);
             }
+        }
+    }
+
+    private static void printArrElements (int i){
+        if (i == (arrNumbers.length - 1)) {
+            System.out.print(arrNumbers[i] + ".");
+        } else {
+            System.out.print(arrNumbers[i] + "; ");
         }
     }
 }
